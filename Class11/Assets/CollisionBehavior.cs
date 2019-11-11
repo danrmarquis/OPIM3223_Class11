5513cd31229d438bc837b7bcf08768b1ed5802a8
alianapilao <aliana.pilao@uconn.edu>
2019-11-11 18:36:58 -0500

    Class 11 Changes

---
+++ Class11/Assets/CollisionBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBehavior : MonoBehaviour
+{
+    public bool collided;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (!collided)
+        {
+            collided = true;
+            //3
+            GameObject clown = gameObject.transform.parent.gameObject;
+            Animator clownAnimator = clown.GetComponent<Animator>();
+            //4
+            clownAnimator.SetTrigger("hit");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
